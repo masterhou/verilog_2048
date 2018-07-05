3db3d01d41fa2cfe90eb87702a116879311cee7f
Thomas Hou <hytcqq@gmail.com>
2018-07-05 15:08:30 +0800

    good

--- block.v
+++ block.v
@@ -1,4 +1,4 @@
-module block #(parameter N=4, size=160)
+module block #(parameter N=4)
 (
 	input clk, rst,
 	output wire[9:0] rom_address,
@@ -10,19 +10,33 @@ module block #(parameter N=4, size=160)
 );
 `include "para_define.v"
 
-//wire[2:0] length = char4==0?(char3==0?(char2==0?1:2):3):4;
-
-wire[3:0] i;
-wire[9:0] poxx, xx, yy;
-wire[3:0] char;
+wire[3:0] i, ch;
+wire[9:0] poxx, xx;
+reg[3:0] char[4];
 
 assign xx = (x-posx);
-assign yy = (y-posy);
+assign i = xx[9:5]<N ? xx[9:5] : 0;
+assign ch = char[i];
+assign poxx = posx + `CHAR_WIDETH*i;
+text text(clk, rst, rom_address, rom_data, poxx, posy, x, y, ch, data);
 
-assign i = xx[9:5];
-
-assign char = 2;
-assign poxx = posx - (`CHAR_WIDETH+1)*i;
-text text(clk, rst, rom_address, rom_data, poxx, posy, xx, yy, char, data);
+always @(posedge clk)
+	case(number)
+		0:		{char[0],char[1],char[2],char[3]}<={4'ha,4'ha,4'ha,4'h0};
+		2:		{char[0],char[1],char[2],char[3]}<={4'ha,4'ha,4'ha,4'h2};
+		4:		{char[0],char[1],char[2],char[3]}<={4'ha,4'ha,4'ha,4'h4};
+		8:		{char[0],char[1],char[2],char[3]}<={4'ha,4'ha,4'ha,4'h8};
+		16:	{char[0],char[1],char[2],char[3]}<={4'ha,4'ha,4'h1,4'h6};
+		32:	{char[0],char[1],char[2],char[3]}<={4'ha,4'ha,4'h3,4'h2};
+		64:	{char[0],char[1],char[2],char[3]}<={4'ha,4'ha,4'h6,4'h4};
+		128:	{char[0],char[1],char[2],char[3]}<={4'ha,4'h1,4'h2,4'h8};
+		256:	{char[0],char[1],char[2],char[3]}<={4'ha,4'h2,4'h5,4'h6};
+		512:	{char[0],char[1],char[2],char[3]}<={4'ha,4'h5,4'h1,4'h2};
+		1024:	{char[0],char[1],char[2],char[3]}<={4'h1,4'h0,4'h2,4'h4};
+		2048:	{char[0],char[1],char[2],char[3]}<={4'h2,4'h0,4'h4,4'h8};
+		4096:	{char[0],char[1],char[2],char[3]}<={4'h4,4'h0,4'h9,4'h6};
+		8192:	{char[0],char[1],char[2],char[3]}<={4'h8,4'h1,4'h9,4'h2};
+		default: ;
+	endcase
 
 endmodule
